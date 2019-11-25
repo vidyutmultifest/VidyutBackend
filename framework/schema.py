42d1512b172e2c0cef308cbd179c6fd2ef23faf0
Ashwin S Shenoy <aswinshenoy65@gmail.com>
2019-11-25 13:10:59 +0530

    Add Google Auth Support

--- framework/schema.py
+++ framework/schema.py
@@ -1,11 +1,37 @@
 import graphene
 import graphql_jwt
+import graphql_social_auth
+
+
+class Query(graphene.ObjectType):
+    users = graphene.Field(graphene.Boolean)
+
+
+class UserType(graphene.ObjectType):
+    username = graphene.Field(graphene.String)
+
+
+class SocialAuth(graphql_social_auth.SocialAuthJWT):
+    user = graphene.Field(UserType)
+    token = graphene.String()
+
+    @classmethod
+    def resolve(cls, root, info, social, **kwargs):
+        try:
+            from graphql_jwt.shortcuts import get_token, get_refresh_token, create_refresh_token
+        except ImportError:
+            raise ImportError(
+                'django-graphql-jwt not installed.\n'
+                'Use `pip install \'django-graphql-social-auth[jwt]\'`.')
+        print(get_refresh_token(social.user))
+        return cls(user=social.user, token=get_token(social.user))
 
 
 class Mutation(graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
+    social_auth = SocialAuth.Field()
 
 
-schema = graphene.Schema(mutation=Mutation)
+schema = graphene.Schema(mutation=Mutation, query=Query)
--- framework/urls.py
+++ framework/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from django.conf.urls import url, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('', include('social_django.urls', namespace='social')),
     path('', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
