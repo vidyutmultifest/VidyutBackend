96fbf22a821c6dd2bfc9e6df90b1bcc515ca4f0d
Ashwin S Shenoy <aswinshenoy65@gmail.com>
2020-01-24 11:02:04 +0530

    Make export script send email with attachment

--- payment/management/commands/export-for-acrd.py
+++ payment/management/commands/export-for-acrd.py
@@ -1,5 +1,8 @@
 import csv
 import json
+from io import StringIO
+from django.core.mail import EmailMessage
+
 from django.core.management.base import BaseCommand
 
 
@@ -11,10 +14,13 @@ from_email = settings.EMAIL_HOST_USER
 
 
 class Command(BaseCommand):
-    help = 'Exports & Sends Registration form applications to an email as a CSV'
+    help = 'Exports & Sends online payment list to an email as a CSV'
 
+    def add_arguments(self, parser):
+        parser.add_argument('email', type=str)
 
     def handle(self, *args, **options):
+        senderEmail = options['email']
         transactions = Transaction.objects.filter(isPaid=True, isOnline=True)
         list = []
         for t in transactions:
@@ -48,9 +54,30 @@ class Command(BaseCommand):
                     timestamp,
                     productName
                 ])
-                print('a')
+                print([
+                    transactionID,
+                    bankrefno,
+                    name,
+                    email,
+                    phone,
+                    college,
+                    amount,
+                    timestamp,
+                    productName
+                ])
             except Order.DoesNotExist:
                 pass
-        with open('acrd-list.csv', 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(list)
+
+        csvfile = StringIO()
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerows(list)
+
+        email = EmailMessage(
+            "Online Payment List for ACRD",
+            'Please find the attachment.',
+            from_email,
+            ['web@vidyut.amrita.edu', senderEmail],
+        )
+        email.attach('online-payment-list.csv', csvfile.getvalue(), 'text/csv')
+        email.send()
+
