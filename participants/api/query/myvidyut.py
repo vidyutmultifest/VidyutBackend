54930d21586fb5ac031c482ec204cfc316dd80c0
Ashwin S Shenoy <aswinshenoy65@gmail.com>
2020-01-29 15:53:04 +0530

    Update MyVidyut API with schedule and contact details

--- participants/api/query/myvidyut.py
+++ participants/api/query/myvidyut.py
@@ -1,12 +1,33 @@
 import graphene
+from django.utils import timezone
 from graphql_jwt.decorators import login_required
 
+from events.models import Workshop, Competition, WorkshopSchedule, CompetitionSchedule
 from payment.models import Order
 from registrations.models import EventRegistration
+
+to_tz = timezone.get_default_timezone()
+
+
+class MyScheduleobj(graphene.ObjectType):
+    venue = graphene.String()
+    start = graphene.String()
+    end = graphene.String()
+
+    def resolve_venue(self, info):
+        return self.venue
+
+    def resolve_start(self, info):
+        return self.slot.startTime.astimezone(to_tz).strftime("%d/%m, %H:%M")
+
+    def resolve_end(self, info):
+        return self.slot.endTime.astimezone(to_tz).strftime("%d/%m, %H:%M")
 
 
 class MyVidyutItemObj(graphene.ObjectType):
     name = graphene.String()
+    contact = graphene.String()
+    schedule = graphene.List(MyScheduleobj)
 
 
 class MyVidyutObj(graphene.ObjectType):
@@ -17,16 +38,22 @@ class MyVidyutObj(graphene.ObjectType):
     def resolve_workshops(self, info):
         workshops = []
         for t in self['workshops']:
+            workshop = Workshop.objects.get(id=t.event.workshop.id)
             workshops.append({
                 "name": t.event.name,
+                "contact": workshop.contacts.first().name + ' - ' + workshop.contacts.first().phone,
+                "schedule": WorkshopSchedule.objects.filter(event=workshop)
             })
         return workshops
 
     def resolve_competitions(self, info):
         competitions = []
         for t in self['competitions']:
+            competition = Competition.objects.get(id=t.event.competition.id)
             competitions.append({
                 "name": t.event.name,
+                "contact": competition.contacts.first().name + ' - ' + competition.contacts.first().phone,
+                "schedule": CompetitionSchedule.objects.filter(event=competition)
             })
         return competitions
 
