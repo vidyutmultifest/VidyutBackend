94ff328b670d35555132a610b8ba4b64cc6d4dbc
Ashwin S Shenoy <aswinshenoy65@gmail.com>
2020-02-01 07:40:11 +0530

    Add refund support + fix

--- payment/api/query/refund.py
+++ payment/api/query/refund.py
@@ -2,7 +2,7 @@ import graphene
 from graphql_jwt.decorators import login_required
 
 from participants.models import Profile
-from payment.models import OrderProduct
+from payment.models import OrderProduct, Refund
 
 
 class RefundObj(graphene.ObjectType):
@@ -29,12 +29,16 @@ class Query(object):
                     order__products__name__contains='Revel'
                 )
                 if op.count() == 1:
+                    hasRefunded = False
+                    if Refund.objects.filter(transaction=op.first().order.transaction).count() == 1:
+                        hasRefunded = True
                     return RefundObj(
                         userName=user.first_name + ' ' + user.last_name,
                         product=op.first().order.products.first().name,
                         amount=op.first().order.transaction.amount * 0.5,
                         isOnline=op.first().order.transaction.isOnline,
-                        transaction=op.first().order.transaction.transactionID
+                        transaction=op.first().order.transaction.transactionID,
+                        hasRefunded=hasRefunded
                     )
             except OrderProduct.DoesNotExist:
                 return None
